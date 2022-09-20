eedfd60376038f0b9eac53d86e7490d7d9e1b3cc
ss26 <sundarsripada26@gmail.com>
2022-09-20 13:26:21 -0500

    fix: types to ints

--- src/main/main.ino
+++ src/main/main.ino
@@ -80,7 +80,7 @@ float feature_buffer[kFeatureElementCount] = {9.80580370e-01, 9.95648267e-02, 9.
  1.95290008e-02, 4.21546857e-01, 3.44167868e-01, 4.03191949e-03,
  5.31554012e-01, 5.31554012e-01, 2.82550313e-01, 3.32329851e-01,
  5.45095615e-01, 3.44167868e-01};
-float* model_input_buffer = nullptr;
+int8_t* model_input_buffer = nullptr;
 }  // namespace
 
 // The name of this function is important for Arduino compatibility.
@@ -141,12 +141,12 @@ void setup() {
   if ((model_input->dims->size != 2) || (model_input->dims->data[0] != 1) ||
       (model_input->dims->data[1] !=
        (kFeatureSliceCount * kFeatureSliceSize)) ||
-      (model_input->type != kTfLiteFloat32)) {
+      (model_input->type != kTfLiteInt8)) {
     TF_LITE_REPORT_ERROR(error_reporter,
                          "Bad input tensor parameters in model");
     return;
   }
-  model_input_buffer = model_input->data.float;
+  model_input_buffer = model_input->data.int8;
 
   // Prepare to access the audio spectrograms from a microphone or other source
   // that will provide the inputs to the neural network.
